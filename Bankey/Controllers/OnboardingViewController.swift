d04c834d7dbe6bbfaf1c10ec760d5e4c203d3f18
Eren İşgören <erenisgoren@gmail.com>
2024-03-24 15:54:50 +0300

    Create generic onboarding view controller

--- Bankey/Controllers/OnboardingViewController.swift
+++ Bankey/Controllers/OnboardingViewController.swift
@@ -8,22 +8,74 @@
 import UIKit
 
 class OnboardingViewController: UIViewController {
+    
+    let heroImageName: String
+    let titleText: String
+    
+    private let stackView: UIStackView = {
+       
+        let stack = UIStackView()
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.axis = .vertical
+        stack.spacing = 20
+        return stack
+    }()
+    
+    private let imageView: UIImageView = {
+       
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    private let label: UILabel = {
+       
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        imageView.image = UIImage(named: heroImageName)
+        label.text = titleText
 
-        // Do any additional setup after loading the view.
+        layout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
     }
-    */
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
 
+
+extension OnboardingViewController {
+    func layout() {
+        
+        stackView.addArrangedSubview(imageView)
+        stackView.addArrangedSubview(label)
+        view.addSubview(stackView)
+        
+        let stackViewConstraints = [
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ]
+        
+        NSLayoutConstraint.activate(stackViewConstraints)
+    }
 }
